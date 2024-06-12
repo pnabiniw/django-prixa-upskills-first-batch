14a8f62059b782e2cb171caff19d1f6efb7ff701
npaudyal <npaudyal321@gmail.com>
2024-06-12 21:24:32 +0545

    templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home_view, another_view, root_page_view, portfolio_view
+from .views import (home_view, another_view, root_page_view, portfolio_view, 
+                    learning_dtl_view, using_bootstrap_view)
 
 urlpatterns = [
     path("home/", home_view),
     path("another/", another_view),
     path("my-portfolio/", portfolio_view, name="portfolio"),
-    path("", root_page_view)
+    path("learning-dtl/", learning_dtl_view, name="learning_dtl"),
+    path("using-bootstrap/", using_bootstrap_view, name="using_bootstrap"),
+    path("", root_page_view, name="root_page")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -15,3 +15,36 @@ def root_page_view(request):
 
 def portfolio_view(request):
     return render(request, template_name="portfolio/index.html")
+
+def learning_dtl_view(request):
+    c = {"name": "Prixa Academy", "student": "Jane"}
+    s = [
+        {"name": "Jon", "age": 30, "display": True},
+        {"name": "Alex", "age": 21, "display": False},
+        {"name": "Hary", "age": 23, "display": True},
+        {"name": "Arya", "age": 24, "display": False},
+    ]
+    c.update(students=s)  # {"name": "Prixa Academy", "student": "Jane", "students": []}
+    # Context should always be a dictionary
+    return render(request, template_name="myapp/dtl.html", context=c)
+
+
+# Features from DTTL
+# 1. {% load static %}
+# 2. {% url 'home' %}
+# 3. {% static 'stati/path' %}
+# 4. {{name}}
+# 5. For Loop => {% for each in data %}{% endfor %}
+# 6. If Conditions => {% if condition %}{% endif %}
+#  {% if condition %} {%else%} {% endif %}
+#  {% if condition %} {% elif condition} {%else%} {% endif %}
+
+
+def using_bootstrap_view(request):
+    s = [
+        {"name": "Jon", "age": 30, "email": "jon@email.com", "address": "KTM", "display": True},
+        {"name": "Alex", "age": 21, "email": "alex@email.com", "address": "PKR", "display": False},
+        {"name": "Hary", "age": 23, "email": "hary@email.com", "address": "BKT", "display": True},
+        {"name": "Arya", "age": 24, "email": "arya@email.com", "address": "LTP", "display": False},
+    ]
+    return render(request, template_name="myapp/using_bootstrap.html", context={"students": s})
