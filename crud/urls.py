a30a71ac5a5d24cfe879a5c7aa35050944c15df6
npaudyal <npaudyal321@gmail.com>
2024-06-18 08:55:59 +0545

    student crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import classroom_view, add_classroom_view
+from .views import (classroom_view, add_classroom_view, update_classroom_view, delete_classroom_view, 
+student_view, add_student_view)
 
 
 urlpatterns = [
     path("classroom/", classroom_view, name="crud_classroom"),
-    path("add-classroom/", add_classroom_view, name="add_classoom")
+    path("student/", student_view, name="crud_student"),
+    path("add-classroom/", add_classroom_view, name="add_classoom"),
+    path("add-student/", add_student_view, name="add_student"),
+    path("update-classroom/<int:id>/", update_classroom_view, name="update_classroom"),
+    path("delete-classroom/<int:id>/", delete_classroom_view, name="delete_classroom"),
 ]
--- crud/views.py
+++ crud/views.py
@@ -14,3 +14,40 @@ def add_classroom_view(request):
         Classroom.objects.create(name=classroom_name, section=section)
         return redirect("crud_classroom")
     return render(request, template_name="crud/add_classroom.html")
+
+
+def update_classroom_view(request, id):
+    classroom = Classroom.objects.get(id=id)
+    print(classroom.name)
+    print(classroom.section)
+    if request.method == "POST":
+        classroom_name = request.POST.get("classroom_name")  # This data from HTML form
+        section = request.POST.get("classroom_section")  # This data from HTML form
+        classroom.name = classroom_name  # set data to classroom object
+        classroom.section = section  # set data to classroom object
+        classroom.save()  # This actually commit / store data to the table
+        return redirect("crud_classroom")
+    return render(request, template_name="crud/update_classroom.html", context={"classroom": classroom})
+
+
+def delete_classroom_view(request, id):
+    classroom = Classroom.objects.get(id=id)
+    if request.method == "POST":
+        classroom.delete()
+        return redirect("crud_classroom")
+    return render(request, template_name="crud/delete_confirmation.html", context={"classroom": classroom})
+
+
+def student_view(request):
+    return render(request, template_name="crud/student_list.html", context={"students": Student.objects.all()})
+
+
+def add_student_view(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        email = request.POST.get("email")
+        address = request.POST.get("address")
+        Student.objects.create(name=name, age=age, email=email, address=address)
+        return redirect("crud_student")
+    return render(request, template_name="crud/add_student.html")
